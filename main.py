0472a3fa12653c79dd9877d708f82359c356d7e8
Semi Truck mc Clusterfuck <39673224+SemiTruckmcClusterfuck@users.noreply.github.com>
2021-11-16 08:20:24 +0000

    added multithreading, removed visualisation

--- main.py
+++ main.py
@@ -2,20 +2,22 @@ import mediapipe as mp
 import cv2
 import os
 import sys
+import multiprocessing as multi
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-for filename in os.listdir(sys.path[0] + "/media"):
+def main_process(filename):
     frameNumber = 0
     if filename.endswith(".mp4"):
         f = open('outputs/' + filename[:-4] + '.csv', 'w')
         f.write("frame,hand_number,wrist_x,wrist_y,wrist_z,thumb_cmc_x,thumb_cmc_y,thumb_cmc_z,thumb_mcp_x,thumb_mcp_y,thumb_mcp_z,thumb_ip_x,thumb_ip_y,thumb_ip_z,thumb_tip_x,thumb_tip_y,thumb_tip_z,index_finger_mcp_x,index_finger_mcp_y,index_finger_mcp_z,index_finger_pip_x,index_finger_pip_y,index_finger_pip_z,index_finger_dip_x,index_finger_dip_y,index_finger_dip_z,index_finger_tip_x,index_finger_tip_y,index_finger_tip_z,middle_finger_mcp_x,middle_finger_mcp_y,middle_finger_mcp_z,middle_finger_pip_x,middle_finger_pip_y,middle_finger_pip_z,middle_finger_dip_x,middle_finger_dip_y,middle_finger_dip_z,middle_finger_tip_x,middle_finger_tip_y,middle_finger_tip_z,ring_finger_mcp_x,ring_finger_mcp_y,ring_finger_mcp_z,ring_finger_pip_x,ring_finger_pip_y,ring_finger_pip_z,ring_finger_dip_x,ring_finger_dip_y,ring_finger_dip_z,ring_finger_tip_x,ring_finger_tip_y,ring_finger_tip_z,pinky_mcp_x,pinky_mcp_y,pinky_mcp_z,pinky_pip_x,pinky_pip_y,pinky_pip_z,pinky_dip_x,pinky_dip_y,pinky_dip_z,pinky_tip_x,pinky_tip_y,pinky_tip_z\n")
+        cap = cv2.VideoCapture("media/out/" + filename)
         print(filename)
-        cap = cv2.VideoCapture("media/" + filename)
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        out = cv2.VideoWriter('media/out/output.avi', fourcc, 5, (1600, 1280))
+        sys.stdout.flush()
+        fourcc = cv2.VideoWriter_fourcc(*"XVID")
+        out = cv2.VideoWriter("media/curr/"+ filename[:-4] +".avi", fourcc, 5, (1600, 1280))
 
         while True:
             ret, frame = cap.read()
@@ -29,7 +31,7 @@ for filename in os.listdir(sys.path[0] + "/media"):
         cap.release()
         out.release()
 
-        cap = cv2.VideoCapture("media/out/output.avi")
+        cap = cv2.VideoCapture("media/curr/"+ filename[:-4] +".avi")
 
         with mp_hands.Hands(
                 model_complexity=0,
@@ -38,7 +40,8 @@ for filename in os.listdir(sys.path[0] + "/media"):
             while cap.isOpened():
                 success, image = cap.read()
                 if not success:
-                    print("Ignoring empty camera frame.")
+                    print("Finished.")
+                    sys.stdout.flush()
                     # If loading a video, use 'break' instead of 'continue'.
                     break
 
@@ -61,19 +64,25 @@ for filename in os.listdir(sys.path[0] + "/media"):
                                     "," + str(x.z) + ",")
                         f.write("\n")
                         hand_number += 1
-                        mp_drawing.draw_landmarks(
-                            image,
-                            hand_landmarks,
-                            mp_hands.HAND_CONNECTIONS,
-                            mp_drawing_styles.get_default_hand_landmarks_style(),
-                            mp_drawing_styles.get_default_hand_connections_style())
-                # Flip the image horizontally for a selfie-view display.
-                cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
                 frameNumber += 1
-                if cv2.waitKey(5) & 0xFF == 27:
+                if cv2.waitKey(1) & 0xFF == 27:
                     break
         cap.release()
         f.close()
+        os.remove("media/curr/"+ filename[:-4] +".avi")
 
-    else:
-        continue
+if __name__ == "__main__":
+    total_files = os.listdir(sys.path[0] + "/media/out")
+    thread_count = 2
+    iterations = len(total_files)//thread_count
+    pool = multi.Pool(multi.cpu_count())
+    # example
+    # 250/5 = 50
+    # -> 50 runs required
+    # -> overall range: 50 >> iterations
+    # -> range inside range: 5 >> thread_count
+    # thread_count has to be dividable by the total number of videos
+    for x in range(iterations):
+        print("Iteration: "+str(x))
+        video_number = x*thread_count
+        pool.map(main_process, [total_files[video_number+y] for y in range(thread_count)])
